9c9f42517ff9498777e1bd91e67f9dbe714fd2fd
Klimkov-M <M.Y.Klimkov@gmail.com>
2024-10-24 13:28:49 +0300

    Отформатипровали вывод

--- currency exchange rate.py
+++ currency exchange rate.py
@@ -15,7 +15,7 @@ def exchange():
             #print (data)
             if code in data['rates']:
                 ex_rates=data['rates'][code]
-                mb.showinfo('Курс обмена', f'Курс к рублю {ex_rates}')
+                mb.showinfo('Курс обмена', f'Курс к рублю {ex_rates:.2f} {code} за один рубль')
         except Exception as err:
             mb.showerror('ERROR!',err)
     else:
@@ -27,7 +27,7 @@ pop_cur = ["EUR", "JPY", "GBP", "AUD", "CAD", "CHF", "CNY", "RUB", "KZT", "UZS"]
 w = Tk()
 w.geometry('600x400')
 
-Label(text='Введите код валюты').pack(pady = (10,0))
+ttk.Label(text='Введите код валюты').pack(pady = (10,0))
 combobox = ttk.Combobox(values=pop_cur);combobox.pack(pady = (10,0))
 
 ttk.Button(text='Получить курс', command=exchange).pack(pady = (10,0))
